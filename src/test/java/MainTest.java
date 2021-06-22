ecd7db61bc712705219b211856d33dcbe03d0c45
arukiidou <koyama.junya@tis.co.jp>
2021-06-23 06:17:00 +0900

    add output

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +11,6 @@ class MainTest {
     @Test
     void getList() throws SQLException {
         Assertions.assertEquals(Main.getList().stream().count(),107);
+        System.out.println(Main.getList().stream().collect(Collectors.summarizingInt(e->e)));
     }
 }
